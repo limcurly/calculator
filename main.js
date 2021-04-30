e2ad05ccae7e75b7a208e09c5d74982438e8e9a2
limcurly <liamcarla00@gmail.com>
2021-04-30 15:18:52 +1000

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+let container = document.getElementById('container');
+let display = document.getElementById('display');
+let buttons = document.getElementById('num-buttons');
+let operators = document.getElementById('operators');
+
+// basic math functions
+
+function add(num, num) {
+     return num + num;
+ }
+
+ function subtract(num, num) {
+    return num - num;
+}
+
+function multiply(num, num) {
+    return num * num;
+}
+
+function divide(num, num) {
+    return num / num;
+}
+
+// function takes two numbers and the operator and performs the function
+
+function operate() {
+
+}
